6c2ad42902f1fe0e8e40afdb79b08db369480a1d
Полина <polina@MacBook-Air-Polina-3.local>
2026-06-06 21:04:18 +0300

    service0605

--- django_rms_hotel/settings.py
+++ django_rms_hotel/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_rms_hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hotel_db',
+        'USER': 'polina',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
